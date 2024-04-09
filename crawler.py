a0b7e83e7af2a1a9b825bf8e5d3db1c27014513b
Joshua <160927816+joshuaDeal@users.noreply.github.com>
2024-04-09 13:22:22 -0400

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+# A webcrawler. It start with a list of root pages and parses each pages's html for new urls to parse. It outputs the urls it discovers to a text file called "raw-urls.txt"
+
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from collections import deque
+
+# Returns the html content from a provided url.
+def getHtml(url):
+	# Find out what the url scheme is so we can make sure that it's valid.
+	scheme = urlparse(url).scheme
+
+	# Check if the URL scheme is HTTP or HTTPS
+	if scheme not in ['http', 'https']:
+		print(f"Skipping URL with unsupported scheme: {url}")
+		return None
+
+	try:
+		htmlContent = None
+		response = requests.get(url)
+		
+		if response.status_code == 200:
+			htmlContent = response.content
+		else:
+			print(f'Failed to fetch URL: {url}')
+			print(f'Status code: {response.status_code}')
+		return htmlContent
+	# In case of an ssl related error.
+	except requests.exceptions.SSLError as err:
+		print(f"SSL Error occurred for URL: {url}")
+		return None
+	# In case of a request execption related error.
+	except requests.exceptions.RequestException as req_err:
+		print(f"Request Exception occurred for URL: {url} - {req_err}")
+		return None
+
+# Apends the url to the url file.
+def updateIndex(fileName, url):
+	with open(fileName, 'a') as file:
+		file.writelines(url)
+		file.write('\n')
+
+# Extracts urls from html.
+def getUrls(htmlContent, baseUrl):
+	urls = []
+	soup = BeautifulSoup(htmlContent, 'html.parser')
+	for link in soup.find_all('a', href=True):
+		url = link.get('href')
+		# Check if url is absolute or relative.
+		if url.startswith('http') or url.startswith('https'):
+			urls.append(url)
+		# If the url is relative, use urljoin() to make it an absolute url.
+		else:
+			fullUrl = urljoin(baseUrl, url)
+			urls.append(fullUrl)
+	return urls
+
+def main():
+	# Initialize a queue with the starting URL(s)
+	url_queue = deque(["https://old.reddit.com","https://en.wikipedia.org/wiki/Main_Page","https://linuxreviews.org/","https://ebay.com", "https://www.jstor.org/","https://news.ycombinator.com/","https://techcrunch.com/","https://arxiv.org/","https://lobste.rs/","https://thepiratebay10.xyz/"])
+	#url_queue = deque(["https://news.ycombinator.com/"])
+	
+	# Set to store visited URLs to avoid duplicates
+	visited_urls = set()
+	
+	while url_queue:
+		# Get the next URL to crawl
+		current_url = url_queue.popleft()
+		if current_url not in visited_urls:
+			visited_urls.add(current_url)
+			# Fetch HTML content
+			html_content = getHtml(current_url)
+			# Make sure that html content was found
+			if html_content != None:
+				# Extract URLs from the current page
+				new_urls = getUrls(html_content, current_url)
+				for url in new_urls:
+					# Avoid revisiting already processed URLs
+					if url not in visited_urls:
+						# Add new URLs to the queue
+						url_queue.append(url)
+						# Add newly discovered urls to urls file.
+						updateIndex("raw-urls.txt",url)
+						print("Added",url,"to index.")
+
+if __name__ == "__main__":
+	main()
---
+++ extractor.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+# A meta data extractor for html pages. Designed to extract meta data from each entry in a text file that contains a list of urls seperated by newlines.
+
+import requests
+import csv
+import shutil
+import os
+from bs4 import BeautifulSoup
+from crawler import getHtml
+
+# Get a url from the url file.
+def getUrl(fileName, lineNumber):
+	try:
+		with open(fileName, 'r') as file:
+			lines = file.readlines()
+			# Check if the line number is within the range of lines in the file
+			if 0 <= lineNumber < len(lines):
+				# Return the line content without newline character
+				return lines[lineNumber].strip()
+			else:
+				print(f"Line number {lineNumber} is out of range in file {fileName}")
+				return 0
+	except FileNotFoundError:
+		print(f"File '{fileName}' not found")
+		return 0
+
+# Gets relevant metadata from a given url. Returns a dict.
+def getMeta(url):
+	metaData = {'url':url}
+
+	# Get the html content from the url.
+	htmlContent = getHtml(url)
+
+	if htmlContent != None:
+		# Parse html content with BeautifulSoup.
+		soup = BeautifulSoup(htmlContent, 'html.parser')
+
+		# Get the page title.
+		titleTag = soup.title
+		if titleTag:
+			metaData['title'] = titleTag.string
+		else:
+			print("Title tag not found.")
+			metaData['title'] = "Untitled Page"
+
+		# Get meta description.
+		description = soup.find('meta', attrs={'name': 'description'})
+		if description:
+			metaData['description'] = description['content']
+			print("Description: ",metaData['description'])
+		else:
+			print("Meta description not found.")
+			metaData['description'] = "No description provided."
+
+		# Get meta keywords.
+		keyWords = soup.find('meta', attrs={'name': 'keywords'})
+		if keyWords:
+			metaData['keywords'] = keyWords['content']
+		else:
+			print("Meta keywords not found.")
+			metaData['keywords'] = ""
+
+		# Get headers.
+		#headers = soup.find_all(['h1','h2','h3','h4','h5','h6'])
+		#if headers:
+		#	headersText = []
+		#	for i in range(len(headers) - 1):
+		#		text = ''
+		#		current = headers[i]
+		#		next_sibling = current.find_next_sibling()
+		#		while current != headers[i + 1]:
+		#			if current == next_sibling:
+		#				text += current.text
+		#			current = current.find_next()
+		#		headersText.append(text)
+
+		#	metaData['headers'] = str(headersText)
+		#else:
+		#	print("Headers not found")
+		#	metaData['headers'] = ""
+
+		return metaData
+	return None
+
+# Check if given url is already in the csv.
+def checkCsv(fileName, url):
+	with open(fileName, 'r', newline='') as file:
+		reader = csv.DictReader(file)
+		for row in reader:
+			if 'url' in row and row['url'] == url:
+				return True
+	return False
+
+# Create or update a csv file
+def updateCsv(fileName, dataDict):
+	# Define feildnames
+	fieldNames = list(dataDict.keys())
+
+	with open(fileName, 'a+', newline='') as file:
+		writer = csv.DictWriter(file, fieldnames=fieldNames)
+
+		# If the file is empty, we will create the header row.
+		if file.tell() == 0:
+			writer.writeheader()
+
+		# Determine if to update an entry in the csv or if to add a new entry.
+		if checkCsv(fileName,dataDict['url']):
+			# TODO: Update entry.
+			# Do note that I'm going to swich to using a database of some sort and the use of csv is just a placeholder.
+			print("Preexisting entry found. Not updated because update logic is yet to be written.")
+		else:
+			# Add new entry.
+			writer.writerow(dataDict)
+
+def main():
+	urlsFile = "raw-urls.txt"
+
+	i = 0
+	while True:
+		url = getUrl(urlsFile,i)
+		if url == 0:
+			break
+		print("Parsing " + url + "...")
+		meta = getMeta(url)
+		if meta != None:
+			updateCsv("index.csv",meta)
+			print("Added",url,"to csv")
+		i = i + 1
+
+if __name__ == "__main__":
+	main()
