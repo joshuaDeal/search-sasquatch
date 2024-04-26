c8012ea94d46101436d348e49c7b84d7f1b196b8
Joshua Deal <thejdeal@tutanota.com>
2024-04-25 22:19:27 -0400

    Update search.php

--- webroot/search.php
+++ webroot/search.php
@@ -1,24 +1,33 @@
 <?php
 
-function printSite() {
+function printSite($searchString) {
 	echo "<!DOCTYPE html>\n";
 	echo "<html lang=\"en\">\n";
 	echo "	<head>\n";
-	echo "		<title>Results - Spaghetti Search!</title>\n";
+	echo "		<title>" . $searchString . " - Spaghetti Search!</title>\n";
 	echo "		<link rel=\"stylesheet\" href=\"style.css\">\n";
 	echo "		<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\">\n";
-	echo "		<meta name=\"description\" content=\"Results for a search.\">\n";
+	echo "		<meta name=\"description\" content=\"Search results for " . $searchString . "\">\n";
 	echo "		<meta charset=\"utf-8\">\n";
 	echo "		<meta name=\"viewport\" content=\"width=device-width, inital-scale=1\">\n";
 	echo "	</head>\n";
 	echo "	<body>\n";
-	echo "		<p>This is a placeholder</p>\n";
+	echo "		<p>You have searched for \"" . $searchString . "\".</p>\n";
 	echo "	</body>\n";
 	echo "</html>\n";
 }
 
 function main() {
-	printSite();
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$searchString = filter_var($_POST["q"], FILTER_SANITIZE_STRING);
+
+		if (empty($searchString)) {
+			header('Location: http://search.example.com/');
+			exit;
+		}
+
+		printSite($searchString);
+	}
 }
 
 main();
