507b94d08dc1dce737363b8f665b8c5cca2ede68
Joshua Deal <thejdeal@tutanota.com>
2024-04-25 21:47:46 -0400

    Work on front end.

---
+++ webroot/search.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"en\">\n";
+echo "	<head>\n";
+echo "		<title>Results - Spaghetti Search!</title>\n";
+echo "		<link rel=\"stylesheet\" href=\"style.css\">\n";
+echo "		<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\">\n";
+echo "		<meta name=\"description\" content=\"Results for a search.\">\n";
+echo "		<meta charset=\"utf-8\">\n";
+echo "		<meta name=\"viewport\" content=\"width=device-width, inital-scale=1\">\n";
+echo "	</head>\n";
+echo "	<body>\n";
+echo "		<p>This is a placeholder</p>\n";
+echo "	</body>\n";
+echo "</html>\n";
+
+?>
