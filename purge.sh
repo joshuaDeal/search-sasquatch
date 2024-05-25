3422aacc203ae8a2a64407e79ffc9e0be4f97efb
Joshua Deal <thejdeal@tutanota.com>
2024-05-24 21:44:17 -0400

    Change time from 10 minutes to 7 days

--- purge.sh
+++ purge.sh
@@ -15,4 +15,4 @@ DB_USER=`echo $CREDS | cut -d ":" -f 1`
 DB_PASSWORD=`echo $CREDS | cut -d ":" -f 2`
 
 # Run MySQL query to delete items older than 10 minutes
-mariadb -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "DELETE FROM sites WHERE last_visited IS NOT NULL AND last_visited < NOW() - INTERVAL 10 MINUTE;"
+mariadb -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "DELETE FROM sites WHERE last_visited IS NOT NULL AND last_visited < NOW() - INTERVAL 7 DAY;"
